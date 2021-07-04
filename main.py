a688e18673578aab109d23fabf9d0945007383d1
Leslie Williams <Seslie2009@yahoo.com>
2021-07-04 15:49:12 -0700

    Makes a window with a scaled image huzzah!

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ pygame.init()
 width = 800
 height = 600
 game_window = pygame.display.set_mode((width,height))
+background = pygame.image.load('assets/FireHeart.jpg')
+#Reassigns the scaled image into the same variable 
+# --Does have blurring issues. Ideally use the size you are going to use.
+background = pygame.transform.scale(background, (width , height))
+
+
 
 clock = pygame.time.Clock()
 
@@ -19,6 +25,8 @@ def run_game_loop():
                 return
         #The fill take a tuple of r,g,b values (255,255,255) is white
         game_window.fill((255,255,255))
+        #blit for images. Blit takes 2 arguments the image var and a position tuple
+        game_window.blit(background , (0,0))
         #Call update everytime you add draw or color the window
         pygame.display.update()
         #Runs 60 times per second --akin to 60fps Higher number =more demanding lower number = less demanding
